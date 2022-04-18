2cbdd14341a5dba2b2b7abeaf720df311e5a2217
Tejas <62064506+KidCoderT@users.noreply.github.com>
2022-04-18 15:01:53 +0530

    Finished Scraper and basic rendering json

--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://timesofindia.indiatimes.com/"
+IMG_URL = "https://static.toiimg.com/thumb/msid-{},width-500,resizemode-4/{}.jpg"
+
+
 def home(request):
-	data = []
-	return JsonResponse(data, safe=False)
+    data = []
+
+    r = requests.get(BASE_URL)
+    soup = BeautifulSoup(r.content, "html5lib")
+    feeds = soup.find_all("div", class_="col_l_6")  # type: ignore
+
+    for feed in feeds:
+        try:
+            a = feed.find("a")
+            tag = a["href"].split("/")[3]
+            title = a.find("figcaption").get_text()
+            img_id = a["href"].split("/")[-1][:-4]
+            img_url = IMG_URL.format(img_id, img_id)
+            data.append({"title": title, "tag": tag, "img_url": img_url})
+        except Exception as e:
+            print(e)
+
+    return JsonResponse(data, safe=False)
